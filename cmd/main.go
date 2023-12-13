4facb437c200ac0370aac3d00dd5d577f294b032
Thomas Yau <yaucp@connect.hku.hk>
2023-12-13 21:23:07 +0800

    re-organise the files

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"ManeBackend/pb"
-	"context"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 	"net"
 	"os"
@@ -16,42 +14,8 @@ type Service struct {
 	pb.UnimplementedServiceServer
 }
 
-func (s *Service) GetUpdatedURLs(_ context.Context, request *pb.GetUpdatedURLsRequest) (*pb.GetUpdatedURLsResponse, error) {
-	if request.GetVersionTimestamp() == nil {
-		return &pb.GetUpdatedURLsResponse{
-			LatestURLs: nil,
-		}, nil
-	}
-	println(request.GetVersionTimestamp().AsTime().String())
-	ts := request.GetVersionTimestamp().AsTime()
-	println(ts.String())
-	URLs := make(map[string]string)
-	URLs["home"] = "12312f.com"
-	LatestUrls := &pb.URLsList{
-		VersionTimestamp: timestamppb.Now(),
-		URLs:             URLs,
-	}
-	return &pb.GetUpdatedURLsResponse{
-		LatestURLs: LatestUrls,
-	}, nil
-}
-
 type HealthCheck struct {
 	pb.UnimplementedHealthServer
-}
-
-func (s *HealthCheck) Check(_ context.Context, request *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
-	log.Printf("Recevied healthcheck request %v", request.GetService())
-	return &pb.HealthCheckResponse{
-		Status: pb.HealthCheckResponse_SERVING,
-	}, nil
-}
-
-func (s *HealthCheck) Watch(_ *pb.HealthCheckRequest, stream pb.Health_WatchServer) error {
-	err := stream.Send(&pb.HealthCheckResponse{
-		Status: pb.HealthCheckResponse_SERVING,
-	})
-	return err
 }
 
 func main() {
@@ -59,15 +23,15 @@ func main() {
 	if !exists {
 		PORT = "8080"
 	}
-	log.Printf("Started main, will be listening at %v", PORT)
+	log.Printf("Read PORT env, will be listening at %v", PORT)
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", PORT))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 	s := grpc.NewServer()
 	pb.RegisterServiceServer(s, &Service{})
 	pb.RegisterHealthServer(s, &HealthCheck{})
+
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(listener); err != nil {
---
+++ cmd/route.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"ManeBackend/pb"
+	"golang.org/x/net/context"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+)
+
+func (s *HealthCheck) Check(_ context.Context, request *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
+	log.Printf("Recevied healthcheck request %v", request.GetService())
+	return &pb.HealthCheckResponse{
+		Status: pb.HealthCheckResponse_SERVING,
+	}, nil
+}
+
+func (s *HealthCheck) Watch(_ *pb.HealthCheckRequest, stream pb.Health_WatchServer) error {
+	err := stream.Send(&pb.HealthCheckResponse{
+		Status: pb.HealthCheckResponse_SERVING,
+	})
+	return err
+}
+
+func (s *Service) GetUpdatedURLs(_ context.Context, request *pb.GetUpdatedURLsRequest) (*pb.GetUpdatedURLsResponse, error) {
+	if request.GetVersionTimestamp() == nil {
+		return &pb.GetUpdatedURLsResponse{
+			LatestURLs: nil,
+		}, nil
+	}
+	println(request.GetVersionTimestamp().AsTime().String())
+	ts := request.GetVersionTimestamp().AsTime()
+	println(ts.String())
+	URLs := make(map[string]string)
+	URLs["home"] = "12312f.com"
+	LatestUrls := &pb.URLsList{
+		VersionTimestamp: timestamppb.Now(),
+		URLs:             URLs,
+	}
+	return &pb.GetUpdatedURLsResponse{
+		LatestURLs: LatestUrls,
+	}, nil
+}
